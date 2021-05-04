e2b46fdfd56b4fb928a793f87e15072a76b1809c
Christian Weirum <Christian.Weirum@outlook.dk>
2021-05-04 14:58:21 +0200

    Last Commit of the day!
    Started to create agent creation so that I can show off some Regex at least. Also tomorow work souly on CRUD

---
+++ ThePetExamn/ThePetExamn/View/CreationView/CreateAgent.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ThePetExamn.View.CreationView
+{
+    /// <summary>
+    /// Interaction logic for CreateAgent.xaml
+    /// </summary>
+    public partial class CreateAgent : Window
+    {
+        public CreateAgent()
+        {
+            InitializeComponent();
+        }
+    }
+}
